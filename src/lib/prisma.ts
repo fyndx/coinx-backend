52286e4abd4979955d72779fd6e7791466df980f
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2026-01-28 17:34:59 +0530

    refactor: update PrismaClient import path by removing .js extension

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaClient } from "../../generated/prisma/client";
 import { env } from "./env";
 
 const pool = new pg.Pool({
